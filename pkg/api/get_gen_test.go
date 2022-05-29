b84baa95e72f1590b7590d20fbf262d7794712dd
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-05-29 09:47:23 +0700

    a

--- pkg/api/get_gen_test.go
+++ pkg/api/get_gen_test.go
@@ -6,48 +6,54 @@ import (
 )
 
 func TestGetGeneration(t *testing.T) {
+	var panoid string
 	var got int
 	var gotErr error
 	var want int
 	var wantErr error
 
-	got, gotErr = GetGeneration("invalidID")
+	panoid = "invalidID"
 	want, wantErr = 0, errors.New("panoid does not exist!")
+	got, gotErr = GetGeneration(panoid)
 
-	if got != want || gotErr == wantErr {
-		t.Fatalf(`getGeneration("invalidID") = %v, %v, want match for %v, %v`, got, gotErr, 0, wantErr)
+	if got != want || gotErr != wantErr {
+		t.Fatalf(`getGeneration("%v") = %v, %v, want match for %v, %v`, panoid, got, gotErr, 0, wantErr)
 	}
 
 	// gen 1 panoid
-	got, gotErr = GetGeneration("_drdwKa3KNQxUR6O7ZUCmQ")
+	panoid = "_drdwKa3KNQxUR6O7ZUCmQ"
+	got, gotErr = GetGeneration(panoid)
 	want, wantErr = 1, nil
 
 	if got != want || gotErr != wantErr {
-		t.Fatalf(`getGeneration("_drdwKa3KNQxUR6O7ZUCmQ") = %v, %v, want match for %v, %v`, got, gotErr, want, wantErr)
+		t.Fatalf(`getGeneration("%v") = %v, %v, want match for %v, %v`, panoid, got, gotErr, want, wantErr)
 	}
 
 	// gen 2 panoid
-	got, gotErr = GetGeneration("hLPja2vrvr5g-6gi3f8-FQ")
+	panoid = "hLPja2vrvr5g-6gi3f8-FQ"
+	got, gotErr = GetGeneration(panoid)
 	want, wantErr = 2, nil
 
 	if got != want || gotErr != wantErr {
-		t.Fatalf(`getGeneration("hLPja2vrvr5g-6gi3f8-FQ") = %v, %v, want match for %v, %v`, got, gotErr, want, wantErr)
+		t.Fatalf(`getGeneration("%v") = %v, %v, want match for %v, %v`, panoid, got, gotErr, want, wantErr)
 	}
 
 	// gen 3 panoid
 	// temporarily, this should return 2 (because they have the same x length)
-	got, gotErr = GetGeneration("ga9K9_YYEwxb4p5ApmQSkA")
+	panoid = "ga9K9_YYEwxb4p5ApmQSkA"
+	got, gotErr = GetGeneration(panoid)
 	want, wantErr = 2, nil
 
 	if got != want || gotErr != wantErr {
-		t.Fatalf(`getGeneration("ga9K9_YYEwxb4p5ApmQSkA") = %v, %v, want match for %v, %v`, got, gotErr, want, wantErr)
+		t.Fatalf(`getGeneration("%v") = %v, %v, want match for %v, %v`, panoid, got, gotErr, want, wantErr)
 	}
 
 	// gen 4 panoid
-	got, gotErr = GetGeneration("9ApA6xOofv6Dq-BrQrdGbQ")
+	panoid = "9ApA6xOofv6Dq-BrQrdGbQ"
+	got, gotErr = GetGeneration(panoid)
 	want, wantErr = 4, nil
 
 	if got != want || gotErr != wantErr {
-		t.Fatalf(`getGeneration("9ApA6xOofv6Dq-BrQrdGbQ") = %v, %v, want match for %v, %v`, got, gotErr, want, wantErr)
+		t.Fatalf(`getGeneration("%v") = %v, %v, want match for %v, %v`, panoid, got, gotErr, want, wantErr)
 	}
 }
