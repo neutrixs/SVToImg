2aa814e0192c216e5d2f6ca32e1129b36b36a8ca
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-05-30 23:07:16 +0700

    not so repetitive tests and add 1 more

--- pkg/api/linktopanoid_test.go
+++ pkg/api/linktopanoid_test.go
@@ -3,59 +3,23 @@ package api
 import "testing"
 
 func TestShortlinkToPanoid(t *testing.T) {
-	var url string
-	var wantPanoid string
-	var wantErr error
-	var gotPanoid string
-	var gotErr error
+	test := func(url string, wantPanoid string, wantErr error) {
+		gotPanoid, gotErr := ShortlinkToPanoid(url)
 
-	url = "https://goo.gl/maps/v3V1nGaqSHuxHpF17"
-	wantPanoid = "jpmaYUuc5-QXvTD29Kz54A"
-	wantErr = nil
-
-	gotPanoid, gotErr = ShortlinkToPanoid(url)
-
-	if gotPanoid != wantPanoid || gotErr != wantErr {
-		t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
+		if gotPanoid != wantPanoid || gotErr != wantErr {
+			t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
+		}
 	}
 
-	url = "https://www.google.com/maps/@-6.256829,106.9520042,3a,75y,277.25h,77.76t/data=!3m6!1e1!3m4!1scP2u46sSj0aUXmyfFCYRWA!2e0!7i16384!8i8192"
-	wantPanoid = "cP2u46sSj0aUXmyfFCYRWA"
-	wantErr = nil
+	test("https://goo.gl/maps/v3V1nGaqSHuxHpF17", "jpmaYUuc5-QXvTD29Kz54A", nil)
+	test("https://goo.gl/maps/mQac1a9wFkt3QRHo7", "JSTF9BICkKl76cYXG5pQvQ", nil)
+	test("random", "", InvalidURLError)
+	test("https://google.com/maps", "", InvalidSVURLError)
+	test("https://example.com/data=!3m6!1e1!3m4!1stest", "", InvalidSVURLError)
 
-	gotPanoid, gotErr = ShortlinkToPanoid(url)
-
-	if gotPanoid != wantPanoid || gotErr != wantErr {
-		t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
-	}
-
-	url = "random"
-	wantPanoid = ""
-	wantErr = InvalidURLError
-
-	gotPanoid, gotErr = ShortlinkToPanoid(url)
-
-	if gotPanoid != wantPanoid || gotErr != wantErr {
-		t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
-	}
-
-	url = "https://google.com/maps"
-	wantPanoid = ""
-	wantErr = InvalidSVURLError
-
-	gotPanoid, gotErr = ShortlinkToPanoid(url)
-
-	if gotPanoid != wantPanoid || gotErr != wantErr {
-		t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
-	}
-
-	url = "https://example.com/data=!3m6!1e1!3m4!1stest"
-	wantPanoid = ""
-	wantErr = InvalidSVURLError
-
-	gotPanoid, gotErr = ShortlinkToPanoid(url)
-
-	if gotPanoid != wantPanoid || gotErr != wantErr {
-		t.Fatalf(`ShortLinkToPanoid("%v") = %v, %v, want match for %v, %v`, url, gotPanoid, gotErr, wantPanoid, wantErr)
-	}
+	test(
+		"https://www.google.com/maps/@-6.256829,106.9520042,3a,75y,277.25h,77.76t/data=!3m6!1e1!3m4!1scP2u46sSj0aUXmyfFCYRWA!2e0!7i16384!8i8192",
+		"cP2u46sSj0aUXmyfFCYRWA",
+		nil,
+	)
 }
