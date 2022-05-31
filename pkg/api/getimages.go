65f879e02e4c5207f29818b0913f4d3b1d2a748a
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-05-31 15:10:24 +0700

    resize img

--- pkg/api/getimages.go
+++ pkg/api/getimages.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"sync"
+
+	"github.com/nfnt/resize"
 )
 
 var EmptyImage *image.RGBA = image.NewRGBA(
@@ -78,7 +80,7 @@ func GetImages(panoid string) (*image.RGBA, error) {
 		imagesYX[y] = map[int]image.Image{}
 		for x := 0; x < config.xAmount; x++ {
 			wg.Add(1)
-			go getImage(&imagesYX, &wg, panoid, y, x, config.zoomLevel, total, &downloaded)
+			go getImage(&imagesYX, &wg, config, panoid, y, x, total, &downloaded)
 		}
 	}
 
@@ -89,7 +91,7 @@ func GetImages(panoid string) (*image.RGBA, error) {
 	return EmptyImage, nil
 }
 
-func getImage(imagesYX *map[int]map[int]image.Image, wg *sync.WaitGroup, panoid string, y, x, zoom, total int, downloaded *int) {
+func getImage(imagesYX *map[int]map[int]image.Image, wg *sync.WaitGroup, config EachgenData, panoid string, y, x, total int, downloaded *int) {
 	defer wg.Done()
 	defer func() {
 		*downloaded++
@@ -97,7 +99,7 @@ func getImage(imagesYX *map[int]map[int]image.Image, wg *sync.WaitGroup, panoid 
 	}()
 
 	baseURL := "https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=maps_sv.tactile&panoid=%v&x=%d&y=%d&zoom=%d&nbt=1&fover=2"
-	URL := fmt.Sprintf(baseURL, panoid, x, y, zoom)
+	URL := fmt.Sprintf(baseURL, panoid, x, y, config.zoomLevel)
 
 	res, err := http.Get(URL)
 
@@ -109,6 +111,12 @@ func getImage(imagesYX *map[int]map[int]image.Image, wg *sync.WaitGroup, panoid 
 
 	if err != nil {
 		log.Fatalln(err)
+	}
+
+	imageWidth := resImage.Bounds().Dx()
+
+	if imageWidth != config.tileResolution {
+		resImage = resize.Resize(uint(config.tileResolution), 0, resImage, resize.Lanczos3)
 	}
 
 	(*imagesYX)[y][x] = resImage
